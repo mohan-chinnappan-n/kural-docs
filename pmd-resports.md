74e2670670f8b8caef3ecb170c27481c22bc9510
mohan-chinnappan-n <mohan.chinnappan.n@gmail.com>
2022-09-09 23:03:19 -0400

    pmd reports added

---
+++ pmd-resports.md
@@ -0,0 +1,3 @@
+# Sample PMD scan report
+
+- [PMD Report](img/pmd-report-1.gif)
---
+++ run-validation.sh
@@ -0,0 +1,1 @@
+sfdx mohanc:mdapi:deploy -u  mohan.chinnappan.n_ea2@gmail.com -z 09S3h0000073HmBEAU.zip.txt -o options.json
